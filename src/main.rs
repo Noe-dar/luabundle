0ed3d4b2fec0d8047c3b4c02521fa0bec26ceb4e
Raddeonn <radeonchik.new@gmail.com>
2023-08-15 21:20:25 +0300

    feat: option to specify the output file name

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,16 @@ use argh::FromArgs;
 use bundler::Bundler;
 use std::{fs::File, path::PathBuf};
 
-#[derive(FromArgs)]
+#[derive(FromArgs, Clone, Debug)]
 /// Bundle lua code
 pub struct BundlerArgs {
-    /// input file
+    /// input file path
     #[argh(positional)]
     pub input: PathBuf,
+
+    /// output name
+    #[argh(option, short = 'o')]
+    pub output: Option<String>,
 }
 
 fn main() -> Result<()> {
@@ -26,7 +30,7 @@ fn main() -> Result<()> {
 
     let bundler_args = argh::from_env::<BundlerArgs>();
 
-    let file = File::create("bundle.lua")?;
+    let file = File::create(bundler_args.output.unwrap_or("bundle.lua".to_string()))?;
 
     let mut bundler = Bundler::new(file);
     bundler.bundle(bundler_args.input)?;
